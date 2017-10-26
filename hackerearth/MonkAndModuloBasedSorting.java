6f71c75389f7bf8bba96426fcabad8b5b27286aa
Ashish <ashishwt@gmail.com>
2017-10-26 14:26:43 +0530

    https://www.hackerearth.com/practice/algorithms/sorting/merge-sort/practice-problems/algorithm/monk-and-modulo-based-sorting/

---
+++ hackerearth/MonkAndModuloBasedSorting.java
@@ -0,0 +1,52 @@
+package hackerearth.codemonk.sorting;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class MonkAndModuloBasedSorting {
+    // java contest template
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        TaskD solver = new TaskD();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class TaskD {
+
+        public void solve(int testNumber, InputReader in, PrintWriter out) {
+            System.out.println(in.nextInt()*2);
+            System.out.println(in.next());
+        }
+
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
